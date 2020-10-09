5f680314247714885376c8a23d3f0b70e6b0709e
Sbsseo <sbsseo7@gmail.com>
2020-10-09 23:07:27 +0530

    Updates

---
+++ precache-manifest.aaf5b2b054b861dbb9a61b7cc8ef184d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f808907c0627100b917f207924ca88",
+    "url": "/mumtajreatlive/index.html"
+  },
+  {
+    "revision": "efe625136dba1ed5d6a1",
+    "url": "/mumtajreatlive/static/css/main.112cd274.chunk.css"
+  },
+  {
+    "revision": "8533e15946888a2feea9",
+    "url": "/mumtajreatlive/static/js/2.1115e20b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/mumtajreatlive/static/js/2.1115e20b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe625136dba1ed5d6a1",
+    "url": "/mumtajreatlive/static/js/main.cf12d0cf.chunk.js"
+  },
+  {
+    "revision": "160f17717f7d42b96b5b",
+    "url": "/mumtajreatlive/static/js/runtime-main.a5f5a1d9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/mumtajreatlive/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "3913ae80e68d08401256eef91da79623",
+    "url": "/mumtajreatlive/static/media/mumtaj.3913ae80.jpg"
+  }
+]);
